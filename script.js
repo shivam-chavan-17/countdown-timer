875f0669140f5ffa10623073a165dad247c54e5c
Shivam Chavan <vc19016@gmail.com>
2023-10-03 20:47:46 +0530

    Added more functionality

---
+++ script.js
@@ -0,0 +1,29 @@
+const endDate = "03 October 2023 08:59 PM"
+document.getElementById("end-date").innerText = endDate // date dynamically refect on page
+const inputs = document.querySelectorAll("input")
+
+function clock() {
+    const end = new Date(endDate)
+    const now = new Date()
+    const diff = (end - now) / 1000
+    if (diff < 0) {
+        document.getElementById("endtimer").innerText = "Event is live!!!"
+        return
+    }
+    // Calculate days
+    inputs[0].value = Math.floor((diff / 3600 / 24))
+    // Calculate hours
+    inputs[1].value = Math.floor((diff / 3600) % 24)
+    // Calculate minuts
+    inputs[2].value = Math.floor((diff / 60) % 60)
+    // Calculate seconds
+    inputs[3].value = Math.floor((diff) % 60)
+}
+
+clock()
+
+setInterval(
+    () => {
+        clock()
+    }, 1000
+)
